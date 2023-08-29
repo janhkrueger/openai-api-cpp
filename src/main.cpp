187a9ccd815e6b395b0d42427077088bcab35e31
Jan H. Krüger <janhkrueger@outlook.com>
2023-08-29 11:17:05 +0000

    Rework main file

--- src/main.cpp
+++ src/main.cpp
@@ -12,32 +12,56 @@
 #include <map>
 #include <vector>
 #include "openai.hpp"
+#include <yaml-cpp/yaml.h>
+
+using json = nlohmann::json;
 
 
 int main() {
-  const std::string scriptName = "main";
-  const std::string konfigDatei = "/workspace/configs/openai-cpp.yaml";
 
+  const std::string scriptName = "main";
   auto logger = spdlog::rotating_logger_mt(scriptName,
                 "/workspace/log/batch.log",
                 1048576 * 5, 3);
   logger->flush_on(spdlog::level::info);
-  logger->info(scriptName + " started");
+  logger->info("Started");
 
-    openai::start(); // Will use the api key provided by `OPENAI_API_KEY` environment variable
-    // openai::start("your_API_key", "optional_organization"); // Or you can handle it yourself
+  const std::string konfigDatei = "/workspace/configs/openai-cpp.yaml";
+  YAML::Node config = YAML::LoadFile(konfigDatei);
 
-    auto completion = openai::completion().create(R"({
-        "model": "davinci-002",
-        "prompt": "Say this is a test",
-        "max_tokens": 7,
-        "temperature": 0
-    })"_json); // Using user-defined (raw) string literals
-    std::cout << "Response is:\n" << completion.dump(2) << '\n';
+  // Read the parameters from the config file
+  logger->debug("  Read parameters");
+  json jQuestion;
+  if(config["openai"] && config["openai"]["prompt"] ) {
+    jQuestion["model"] = config["openai"]["model"].as<std::string>();
+    jQuestion["prompt"] = config["openai"]["prompt"].as<std::string>();
+    jQuestion["max_tokens"] = config["openai"]["max_tokens"].as<int>();
+    jQuestion["temperature"] = config["openai"]["temperature"].as<int>();
+  }
+  else {
+    logger->info("Config parameters not found");
+    return 2;
+  }
 
+  logger->debug("  Send to OpenAI");
+  openai::start(); // Will use the api key provided by `OPENAI_API_KEY` environment variable
 
+  // Send the question to OpenAI and store the response
+  auto completion = openai::completion().create(jQuestion);
 
-  logger->info(scriptName + " endet");
+  // Parse JSON data
+  logger->debug("  Parse Response");
+  json j = json::parse(completion.dump(2));
+
+  // Access the "text" attribute
+  std::string text = j["choices"][0]["text"];
+
+  // Output the value and log it
+  logger->debug("  Logging Response");
+  std::cout << "Response: " << text << std::endl;
+  logger->info("Response: " + text);
+
+  logger->info("Endet");
 
   return 0;
 }
